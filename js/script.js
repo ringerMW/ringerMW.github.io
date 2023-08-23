1522cbda0a22f052e2e04f4ec134efad7ab40536
Alexander <141827097+ringerMW@users.noreply.github.com>
2023-08-23 19:50:33 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $('.btn_menu').click(function() {
+      $('.header_nav', ).toggleClass('active');
+      $('.bgr_hide_menu', ).toggleClass('active');
+      $('.subheader_wrap', ).toggleClass('active');
+  });
+});
+
+$(document).ready(function() {
+  $('.btn_nav_close').click(function() {
+    $('.header_nav', ).toggleClass('active');
+    $('.bgr_hide_menu', ).toggleClass('active');
+    $('.subheader_wrap', ).toggleClass('active');
+  });
+});
